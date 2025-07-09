22e5996e0b9629d2ae24c8ad0209549675c47657
Siddharth Negi <siddharth0negi12@gmail.com>
2025-07-09 16:46:50 +0530

    Re-add all files including fixmyroad-frontend

---
+++ Backend/middleware/adminMiddleware.js
@@ -0,0 +1,10 @@
+const User = require("../models/User")
+
+const isAdmin = async(req,res,next)=>{
+    const user=await User.findById(req.user._id);
+    if(!user || !user.isAdmin){
+        return res.status(403).json({message:"Access denied: Admins only"})
+    }
+    next();
+};
+module.exports={isAdmin};
